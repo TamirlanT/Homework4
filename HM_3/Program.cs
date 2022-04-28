416e12eb4523307617c6dbb55d6f0f7bdf811d66
Tamirlan <tuarshintamir@gmail.com>
2022-04-28 17:46:19 +0500

    Complete HM_3

---
+++ HM_3/Program.cs
@@ -0,0 +1,19 @@
+﻿// Напишите программу, которая задаёт массив из некоторого количества элементов
+// и выводит их на экран с помощью функций. 
+//(можно спрашивать количество вводимых элементов и вводить все значения с клавиатуры)
+// 1, 2, 5, 7, 19 -> [1, 2, 5, 7, 19]
+// 6, 1, 33 -> [6, 1, 33]
+
+Console.Write(" Введите количество элементов массива: ");
+int elemetnsCount = Convert.ToInt32(Console.ReadLine());
+int [] myArray = new int [elemetnsCount];
+for (int i = 0; i < myArray.Length; i++)
+{
+    Console.Write($" Введите элемент массива под индексом {i} :");
+    myArray[i] = Convert.ToInt32(Console.ReadLine());
+}
+Console.WriteLine("Вывод Массива");
+for (int i = 0; i < myArray.Length; i++)
+{
+    Console.WriteLine(myArray[i]);
+}
