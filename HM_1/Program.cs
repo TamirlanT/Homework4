645feb6cc339a90f6539836ea7c1ddc52139e1e7
Tamirlan <tuarshintamir@gmail.com>
2022-04-28 16:18:12 +0500

    HM_1 Complete

---
+++ HM_1/Program.cs
@@ -0,0 +1,15 @@
+﻿// Напишите (функцию) цикл, который принимает на вход два числа (A и B) и возводит число A в натуральную степень B.
+
+int Stepen (int A, int B)
+{
+    int result = 1;
+    int i = 1;
+    for (i=1; i<=B; i++)
+    {
+        result = result * A;
+    }
+    return result;
+}
+Console.WriteLine(Stepen(3,5));
+Console.WriteLine();
+Console.WriteLine(Stepen(2,4));
