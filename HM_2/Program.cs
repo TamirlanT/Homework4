937f50452d225d50d5bfb31e074cc7b2058a2ff5
Tamirlan <tuarshintamir@gmail.com>
2022-04-28 17:07:18 +0500

    HM_2 Complete

---
+++ HM_2/Program.cs
@@ -0,0 +1,18 @@
+﻿// Напишите функцию, которая принимает на вход число и выдаёт сумму цифр в числе.
+
+int SumAllN (int N)
+{
+int result = 0;
+while ( N > 0)
+{
+    int sum = N % 10;
+    result = result + sum;
+    N = N/10;
+}
+return result;
+}
+Console.WriteLine(SumAllN(452));
+Console.WriteLine();
+Console.WriteLine(SumAllN(82));
+Console.WriteLine();
+Console.WriteLine(SumAllN(9012));
